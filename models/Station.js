5cef8f255853693710542150a7cfe5b7be71ad42
Hewan Redie <tewol22@gmail.com>
2021-07-22 14:26:52 -0500

    models created for each table

---
+++ models/Station.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Station extends Model {}
+
+Station.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
+  },
+  
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'station',
+  }
+);
+
+module.exports = Station;
---
+++ models/Train.js
@@ -0,0 +1,42 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize train model (table) by extending off Sequelize's Model class
+class Train extends Model {}
+
+// set up fields and rules for Product model
+Train.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    schedle_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'schedule',
+        key: 'id',
+      },
+    },
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'train',
+  }
+);
+
+module.exports = Train;
