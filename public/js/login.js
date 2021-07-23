f3242ab2329ebf25fab0994595b5c56707bc1b73
Zacharyjg <guldb016@umn.edu>
2021-07-23 12:00:48 -0500

    Completed login and lougout .js files

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
     if (email && password) {
         // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/passengers/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
@@ -21,3 +21,34 @@ const loginFormHandler = async (event) => {
         }
     }
 };
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const firstName = document.querySelector('#first-name-signup').value.trim();
+    const lastName = document.querySelector('#last-name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (firstName && lastName && email && password) {
+        const response = await fetch('/api/passengers', {
+            method: 'POST',
+            body: JSON.stringify({ firstName, lastName, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
