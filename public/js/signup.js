e2334486378c863047c9fd413c7d91bbeed4768d
Zacharyjg <guldb016@umn.edu>
2021-07-27 01:38:15 -0500

    Signup now properly works and adds user to the db

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,20 +1,19 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const firstName = document.querySelector('#first-name-signup').value.trim();
-    const lastName = document.querySelector('#last-name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (firstName && lastName && email && password) {
+    if (username && email && password) {
         const response = await fetch('/api/passengers', {
             method: 'POST',
-            body: JSON.stringify({ firstName, lastName, email, password }),
+            body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
