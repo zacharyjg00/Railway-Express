03e103505b27df7a9ea31b6e1d5a15ae1bbceaa2
Zacharyjg <guldb016@umn.edu>
2021-07-28 17:10:09 -0500

    Added updating of user details functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,12 +1,33 @@
-const updateButtonHandler = (event) => {
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
-        document.location.replace(`/update/${id}`);
+const updateButtonHandler = async (event) => {
+    const first_name = document.querySelector('#first-name').value.trim();
+    const last_name = document.querySelector('#last-name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    
+    if (first_name && last_name && email) {
+        if (event.target.hasAttribute('data-id')) {
+            const id = event.target.getAttribute('data-id');
+
+            const response = await fetch(`/api/profile/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({ first_name, last_name, email }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                alert('Successfully updated user details!');
+                document.location.replace('/profile');
+            } else {
+                alert('Failed to update user details');
+            }
+        }
     }
+
 };
 
 const delButtonHandler = async (event) => {
-    
+
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
 
