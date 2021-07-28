111b27f8d29bd79f65ec43a688fe6b208252aede
Zacharyjg <guldb016@umn.edu>
2021-07-28 10:58:25 -0500

    Changed depart_time to a string

--- seeds/station-seeds.js
+++ seeds/station-seeds.js
@@ -3,35 +3,35 @@ const { Station } = require('../models');
 const stationData = [
   {
     name: 'Albert Lea',
-    depart_time: 7
+    depart_time: "7 AM"
   },
   {
     name: 'Faribault',
-    depart_time: 8
+    depart_time: "8 AM"
   },
   {
     name: 'Lakeville',
-    depart_time: 9
+    depart_time: "9 AM"
   },
   {
     name: 'Minneapolis - St. Paul',
-    depart_time: 10
+    depart_time: "10 AM"
   },
   {
     name: 'St. Cloud',
-    depart_time: 11
+    depart_time: "11 AM"
   },
   {
     name: 'Brainerd',
-    depart_time: 12
+    depart_time: "12 PM"
   },
   {
     name: 'Duluth',
-    depart_time: 13
+    depart_time: "1 PM"
   },
   {
     name: 'Bemidji',
-    depart_time: 14
+    depart_time: "2 PM"
   },
 ];
 
