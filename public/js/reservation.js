7358d3ac2c28bdedd9be070b5f6856eb4741a910
Calvin Li <calvinli12345@gmail.com>
2021-07-28 14:27:54 -0500

    added reservation.js

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,17 +1,23 @@
 const reservationFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    // const username = document.querySelector('#username-signup').value.trim();
+    // const email = document.querySelector('#email-signup').value.trim();
+    // const password = document.querySelector('#password-signup').value.trim();
 
     
     if (username && email && password) {
-        const response = await fetch('/models/Reservation', {
+        const response = await fetch('/api/reservation', {
             method: 'POST',
             body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
+    }
+
+    if (response.ok) {
+        document.location.replace('/profile');
+    } else {
+        alert(response.statusText);
     }
 };
 
