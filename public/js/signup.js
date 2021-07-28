b0c9e31f98d25746c53bda5ee121b13253395ae6
Zacharyjg <guldb016@umn.edu>
2021-07-28 14:25:10 -0500

    Added first and last name in place of username

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,15 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
+    const first_name = document.querySelector('#firstname-signup').value.trim();
+    const last_name = document.querySelector('#lastname-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password) {
+    if (first_name && last_name && email && password) {
         const response = await fetch('/api/passengers', {
             method: 'POST',
-            body: JSON.stringify({ username, email, password }),
+            body: JSON.stringify({ first_name, last_name, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
