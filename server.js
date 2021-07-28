402c0dde9c7a6e70c05d7c50776c58bf29cb67c8
Hewan Redie <tewol22@gmail.com>
2021-07-28 12:05:25 -0500

    Test

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// TESTING 123
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
